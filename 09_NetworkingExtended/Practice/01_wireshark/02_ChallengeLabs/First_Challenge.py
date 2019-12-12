c671a567c53240cbda2c62c297ce2f2b0b78fa11
Mitch Hydrick <gary.hydrickm@gmail.com>
2019-12-12 15:36:03 -0600

    completed exercise 1

---
+++ 09_NetworkingExtended/Practice/01_wireshark/02_ChallengeLabs/First_Challenge.py
@@ -0,0 +1,28 @@
+"""
+Open Challenge101-0.pcapng and answer these challenge questions. 
+
+This is to focus on what you can learn about communications based on the main wireshark view.
+
+Q1: How many packets are in this trace file?
+A: 20
+
+Q2: What IP hosts are making a TCP connection in frames 1, 2, and 3?
+A: 192.168.1.108 and 50.19.229.205
+
+Q3: What HTTP command is sent in frame 4?
+A: GET /Tracking/V3/Instream/Impression/?start|2873|72147|75904|9028|26105|
+undefined|1338|3379|807|BBEEND|&iari=148373&cb=634687327792649999&
+internalRedirect=false HTTP/1.1\r\n
+
+Q4: What is the length of the largest frame in this trace file?
+A: 1384
+
+Q5: What protocols are seen in the Protocol column?
+A: HTTP and TCP
+
+Q6: What responses are sent by the HTTP server?
+A: (5) "HTTP/1.1 302 Found" responses
+
+Q7: Is there any IPv6 traffic in this trace file?
+A: No
+"""
