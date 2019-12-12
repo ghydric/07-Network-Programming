7204fb84dff11ec12e88113c84310a14cfcdfd3b
Mitch Hydrick <gary.hydrickm@gmail.com>
2019-12-12 15:38:20 -0600

    added more code to last function

--- Class_Notes/Crypto/encode_base64.py
+++ Class_Notes/Crypto/encode_base64.py
@@ -4,7 +4,8 @@ a-z 26-51
 0-9 52-62
 +,/ 63,64
 """
-
+# the encoded string to decode
+encoded_string = ""
 # function that creates and returns a base64 dictionary with [key = char, value = index]
 def create_base64_dict():
     # setup all base64 characters
@@ -35,9 +36,14 @@ def create_base64_dict():
     return base64_dict
 
 # function that takes in a base64 encoded string and returns a list of correlating indices
-def convert_string_to_indices(enc_str):
+def convert_string_to_indices(enc_str, dict_64):
     # instantiate list of indices
     indices_list = []
+    # loop through each letter in encoded string and add correct index to indices_list
+    for letter in enc_str:
+        indices_list.append(dict_64[letter])
+    return indices_list
+
 
 # function that takes in list of indices and returns a concatenated binary string
 def create_binary_string(ind_list):
@@ -50,3 +56,5 @@ def create_binary_string(ind_list):
 # create the base64 dictionary
 base64 = create_base64_dict()
 
+# get a list of indices for each encoded character
+indices = convert_string_to_indices(encoded_string, base64)
