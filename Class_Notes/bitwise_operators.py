ce612c5332ef14b7bb77e2e379ef99217dcff063
Mitch Hydrick <gary.hydrickm@gmail.com>
2019-12-13 15:09:20 -0600

    updated bitwise_operators.py

---
+++ Class_Notes/bitwise_operators.py
@@ -0,0 +1,37 @@
+"""
+*** Bitwise Operators ***
+  ~  ->  Two's Complement
+  &  ->  AND
+  |  ->  OR
+  ^  ->  XOR
+  << ->  Shift Left
+  >> ->  Shift Right
+
+Truth Tables:
+
+  AND (&)
+-----------
+X  Y | XY
+0  0 | 0
+0  1 | 0
+1  0 | 0
+1  1 | 1
+
+  OR (|)
+-----------
+X  Y | XY
+0  0 | 0
+0  1 | 1
+1  0 | 1
+1  1 | 1
+
+  XOR (^)
+-----------
+X  Y | XY
+0  0 | 0
+0  1 | 1
+1  0 | 1
+1  1 | 0
+
+
+"""
